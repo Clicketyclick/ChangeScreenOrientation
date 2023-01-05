6ebe93f975f0fe914ac61677b64937b87cd8f100
bri <bri@alpine.wsl>
2023-01-05 18:12:18 +0000

    Too many files we don't need so many files

--- ChangeOrientation.ps1.cmd
+++ ChangeOrientation.ps1.cmd
@@ -1,3 +1,12 @@
+<#  :cmd header for PowerShell script
+@   set dir=%~dp0
+@   set ps1="%TMP%\%~n0-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.ps1"
+@   copy /b /y "%~f0" %ps1% >nul
+@   powershell -NoProfile -ExecutionPolicy Bypass -File %ps1% %*
+@   del /f %ps1%
+@   goto :eof
+#>
+
 Function Set-ScreenResolutionAndOrientation { 
 
 <# 
--- co.cmd
+++
@@ -1,5 +0,0 @@
-::powershell -noexit "%~dp0\ChangeOrientation.ps1"
-
-::powershell.exe -noexit "& '%~dp0ChangeOrientation.ps1'"
-
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0ChangeOrientation.ps1"
